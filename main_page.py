10e3d261a963b4b7c6c0d8d52e3232098cf47797
jolayemi_local <ajolayemi82@gmail.com>
2021-08-22 13:25:00 +0200

    Imported all necessary modules and defined all necessary variables

--- main_page.py
+++ main_page.py
@@ -2,3 +2,20 @@
 
 """ Program's main GUI window. """
 
+from PyQt5.QtCore import QThread
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import (QMainWindow, QWidget,
+                             QVBoxLayout, QApplication,
+                             QLabel, QPushButton, QFormLayout,
+                             QMessageBox)
+
+from helper_modules import helper_functions
+from alv_api_communicator import AlvApiThread
+
+MSG_FONT = QFont('Italics', 13)
+BUTTONS_FONT = QFont('Times', 13)
+SPREADSHEET_INFO_JSON_FILE = 'google_sheet_info.json'
+
+JSON_FILE_CONTENT = helper_functions.json_file_loader(
+    file_name=SPREADSHEET_INFO_JSON_FILE
+)
